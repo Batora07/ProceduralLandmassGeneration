c11c0ef65440669120ec8ea705caa2c6cd65110e
Marc Portet <marc@webshaped.fr>
2017-04-06 23:36:11 +0200

    Program is now properly generating meshes procedurally

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
-	public enum DrawMode { NoiseMap, ColourMap };
+	public enum DrawMode { NoiseMap, ColourMap, Mesh };
 	public DrawMode drawMode;
 
 	public int mapWidth;
@@ -53,6 +53,10 @@ public class MapGenerator : MonoBehaviour {
 		else if (drawMode == DrawMode.ColourMap)
 		{
 			display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+		}
+		else if (drawMode == DrawMode.Mesh)
+		{
+			display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
 		}
 	}
 
